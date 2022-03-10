b8ae9ef020280a8aa6ce97c1e55ad3ce7d1c68b9
NikolayKharkov <vtiko564@gmail.com>
2022-03-10 14:17:07 +0300

    1. Spring DI. Запуск [#2091]

---
+++ src/main/java/di/SpringDI.java
@@ -0,0 +1,17 @@
+package di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ConsoleInput ci = context.getBean(ConsoleInput.class);
+        ui.add(ci.askStr("Question ?"));
+        ui.print();
+    }
+}
